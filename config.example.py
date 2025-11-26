5f52646497fa9fd926edb6ffe5b6b21e3f5889a4
beart72 <mtyrin@yandex.ru>
2025-11-26 21:15:45 +0400

    Initial commit: Legal Document Bot with AI features

---
+++ config.example.py
@@ -0,0 +1,6 @@
+class Config:
+    DB_NAME = "legal_bot.db"
+    TOKEN = "YOUR_TELEGRAM_BOT_TOKEN_HERE"
+    ADMIN_IDS = [123456789]  # Замените на ваш Telegram ID
+    USER_CACHE_TIMEOUT = 300
+    AI_CACHE_TIMEOUT = 600
